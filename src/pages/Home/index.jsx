7218b01fdae509233972d6d8016c45e000b309bc
André Victor <andredezzy@outlook.com>
2019-03-29 22:45:56 -0300

    feat(home page): :sparkles: add main card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,13 +5,24 @@ import { HomeContainer } from "./styles";
 import { Container } from "react-bootstrap";
 import Header from "../../components/Header";
 
+import PanelLayoutImage from "../../assets/panel-layout.png";
+
 export default class Home extends Component {
   render() {
     return (
       <HomeContainer>
         <Header />
         <Container className="home-container">
-          <h1>Home Page</h1>
+          <div className="main-card">
+            <p>
+              An open-source project and your friendly application!
+              <br />
+              <br />
+              Stay calm, we'll take care of you :D
+            </p>
+
+            <img src={PanelLayoutImage} alt="panel-layout" />
+          </div>
         </Container>
       </HomeContainer>
     );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,23 +1,49 @@
 import styled from "styled-components";
 
 const HomeContainer = styled.div`
-  background: #f4f5f7;
-
-  h1 {
-    margin: 0;
-  }
+  background: #fff;
+  box-shadow: 0 7px 15px rgba(0, 0, 0, 0.06);
 
   .home-container {
-    padding-top: 20px;
+    .main-card {
+      padding-top: 40px;
+      overflow: hidden;
+      cursor: default;
 
-    h1 {
-      padding-bottom: 20px;
+      p {
+        float: left;
+        margin-top: 50px;
+        color: #005029;
+        width: 42%;
+        font-size: 42px;
+        font-weight: 700;
+        line-height: 45px;
+      }
+
+      .buttons {
+        position: absolute;
+        text-transform: uppercase;
+        bottom: 0;
+
+        .enjoy {
+        }
+      }
+
+      img {
+        float: right;
+        width: 53%;
+      }
     }
   }
 
   @media (min-width: 1200px) {
     .container {
       max-width: 1525px;
+
+      .home-container {
+        img {
+        }
+      }
     }
   }
 `;
